8113829ca4b6d7ae8466da8d6014c87eff56de22
pedronoguera04 <pedro.noguera.sanchez@iescamas.es>
2025-03-13 10:13:56 +0100

    documentacion con JavaDocs

--- Tienda/src/main/java/com/miempresa/entidad/Tienda.java
+++ Tienda/src/main/java/com/miempresa/entidad/Tienda.java
@@ -4,29 +4,42 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * La clase Tienda representa una tienda que contiene una lista de productos.
- * Proporciona métodos para agregar productos, obtener la lista de productos
- * y calcular el precio total de todos los productos en la tienda.
+ * Representa una tienda que contiene una lista de productos.
+ * Permite agregar productos, obtener la lista de productos y calcular el precio total.
  */
 public class Tienda {
     private List<Producto> productos;
 
-  
+    /**
+     * Crea una nueva instancia de Tienda con una lista vacía de productos.
+     */
     public Tienda() {
         this.productos = new ArrayList<>();
     }
 
-
+    /**
+     * Agrega un producto a la tienda.
+     * 
+     * @param p El producto a agregar.
+     */
     public void agregarProducto(Producto p) {
         productos.add(p);
     }
 
-
+    /**
+     * Devuelve una copia de la lista de productos en la tienda.
+     * 
+     * @return Una lista de productos disponibles en la tienda.
+     */
     public List<Producto> obtenerProductos() {
         return new ArrayList<>(productos);
     }
 
-
+    /**
+     * Calcula el precio total de todos los productos en la tienda.
+     * 
+     * @return La suma de los precios de todos los productos.
+     */
     public double calcularPrecioTotal() {
         return productos.stream().mapToDouble(Producto::getPrecio).sum();
     }
